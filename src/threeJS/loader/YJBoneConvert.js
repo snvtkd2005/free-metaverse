f29dae5f11d76532aed27ede056033b711bc6981
RDD05-PC\RDD05 <406729769@qq.com>
2023-10-18 18:01:18 +0800

    准备骨骼转换

---
+++ src/threeJS/loader/YJBoneConvert.js
@@ -0,0 +1,31 @@
+
+
+
+// 骨骼转换
+import * as THREE from "three";
+
+class YJBoneConvert {
+  constructor(model, targetModel) {
+
+    
+    this.Destroy = function(){
+      
+    }
+ 
+    function Init() {
+       
+    } 
+
+    Init();
+
+    this.SetMessage = function (msg) {
+      if (msg == null || msg == undefined || msg == "") { return; }
+
+      console.log(" 设置 骨骼转换 msg = ", msg);
+      
+ 
+    } 
+
+  }
+} 
+export { YJBoneConvert };
